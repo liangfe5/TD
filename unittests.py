94658cf56db88b92e38049ddeab8ce8366211539
liangfe5 <felicia.liang@mail.utoronto.ca>
2020-03-07 14:08:21 -0500

    extra test

--- unittests.py
+++ unittests.py
@@ -13,6 +13,7 @@ class TestTD(unittest.TestCase):
         self.assertEqual('50-00', result)
 
     def test_given(self):
+        # test given examples
         test = ThirtySecondsFormatter()
         result = test.convert(105.25)
         result2 = test.convert(9.03125)
@@ -20,9 +21,16 @@ class TestTD(unittest.TestCase):
         self.assertEqual('105-08', result)
 
     def test_negative(self):
+        # test negative values
         test = ThirtySecondsFormatter()
         result = test.convert(-0.50)
         self.assertEqual('-00-16', result)
+
+    def test_invalid(self):
+        # test invalid values
+        test = ThirtySecondsFormatter()
+        result = test.convert('not a number')
+        self.assertEqual("invalid type", result)
 
 if __name__ == '__main__':
     unittest.main()
