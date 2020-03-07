94834cb39ab08bf32a1b1cb1b7396b7f1153d1fa
liangfe5 <felicia.liang@mail.utoronto.ca>
2020-03-07 14:06:36 -0500

    removed comments

--- unittests.py
+++ unittests.py
@@ -24,22 +24,5 @@ class TestTD(unittest.TestCase):
         result = test.convert(-0.50)
         self.assertEqual('-00-16', result)
 
-
-
-
-    # def test_upper(self):
-    #     self.assertEqual('foo'.upper(), 'FOO')
-    #
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-    #
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
-
 if __name__ == '__main__':
     unittest.main()
